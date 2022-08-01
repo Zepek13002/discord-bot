855fe13304adc7e1958c0a5fa1c67016356ed023
Zepek13002 <110387075+Zepek13002@users.noreply.github.com>
2022-08-01 11:46:25 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+<p align="center">
+<img src="https://readme-typing-svg.herokuapp.com?size=38&color=F7F7F7&center=true&lines=One+For+All">
+</p>
+
+
+---
+À mettre dans le **config.json**
+
+```py
+1. TOKEN = "Le token de votre bot"
+2. PREFIX = "Le préfix"
+3. OWNERS = "Les owners du bot"
+```
+---
+
---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    token: "",
+    prefix: '!',
+    database : {
+        user: '',
+        name: 'wez te bz',
+        password : ''
+    },
+    color: '#36393F',
+    owners: ['281492584176549891', '281492584176549891', '281492584176549891'],
+    botperso: true,
+    topGgToken: ""
+}
